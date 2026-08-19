dcddab9ef2d42f071ce81f026fb8439de86cf033
klungdingbrag <cimunmoses92@gmail.com>
2026-08-19 19:00:11 +0700

    Fix frontend to use the newly deployed Google Apps Script API

--- js/config.js
+++ js/config.js
@@ -1,6 +1,7 @@
 /* Public frontend configuration only. Never put passwords or API secrets here. */
 window.KASRT_CONFIG = {
   appName: 'Kas RT 01 / RW 01',
-  apiUrl: 'https://script.google.com/macros/s/AKfycbyGFrbhCr7lnKURqSLFSr5-lQBN2sQhywIwETNn11ToimYH6WEkTHvT5GKK4Z6lH8DK/exec',
-  demoMode: false
+  apiUrl: 'https://script.google.com/macros/s/AKfycbxDw8MIvUHNR2mu3MLe1h2aW70l14H-V71MGF-jBJHB94g4p3LhOyfGFj2E5L_Z8ANRQQ/exec',
+  demoMode: false,
+  cacheMode: 'no-store'
 };
