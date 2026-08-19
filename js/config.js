8b954f1392d30cc884fdcf5ff13259ec33e1be75
klungdingbrag <cimunmoses92@gmail.com>
2026-08-19 18:15:46 +0700

    Connect frontend to Google Apps Script Web App

--- js/config.js
+++ js/config.js
@@ -1,7 +1,6 @@
 /* Public frontend configuration only. Never put passwords or API secrets here. */
 window.KASRT_CONFIG = {
   appName: 'Kas RT 01 / RW 01',
-  // Isi setelah Web App Google Apps Script berhasil di-deploy.
-  apiUrl: '',
-  demoMode: true
+  apiUrl: 'https://script.google.com/macros/s/AKfycbyGFrbhCr7lnKURqSLFSr5-lQBN2sQhywIwETNn11ToimYH6WEkTHvT5GKK4Z6lH8DK/exec',
+  demoMode: false
 };
